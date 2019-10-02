c380cf9d10166acad48a8122ef2cbbde483712fa
Filipe Oliveira <filipe.soares@ans.gov.br>
2019-10-02 18:49:19 -0300

    Started solution to array left rotation

---
+++ array-left-rotation/src/main/java/com/github/filipesoares/algorithms/array/leftrotation/Solution.java
@@ -0,0 +1,63 @@
+package com.github.filipesoares.algorithms.array.leftrotation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+    	
+    	do {
+    		
+    		// int[] array = new int[a.length];
+    		
+    		for (int j = 0; j < a.length; j++) {
+    			if (j==0)
+    				a[(a.length-1)] = a[j];
+    			
+    			else
+    				a[j-1] = a[j];
+			}
+    		 
+    		d--;
+    		// a = array;
+    		
+    		
+		} while (d>0);
+    	
+    	return a;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        
+    	BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] params = reader.readLine().split(" ");
+        
+        int n = Integer.parseInt(params[0]);
+        int d = Integer.parseInt(params[1]);
+        
+        String[] aItems = reader.readLine().split(" ");
+        
+        int[] result = rotLeft(Arrays
+				.asList(aItems)
+				.stream()
+				.mapToInt(Integer::parseInt).toArray(), d);
+        
+        for (int i = 0; i < result.length; i++) {
+        	bufferedWriter.write(String.valueOf(result[i]) + (i!=(result.length-1) ?  " ": ""));
+		}
+
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        
+    }
+}
