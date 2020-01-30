2725e7d066aa89deb039cebb2d1f6ea99ab03441
Filipe Soares de Oliveira <filipe.oliveira@radixeng.com.br>
2020-01-30 19:26:53 -0300

    Solution of new year of chaos problem

--- new-year-chaos/src/main/java/com/github/filipesoares/algorithms/array/newyearchaos/Solution.java
+++ new-year-chaos/src/main/java/com/github/filipesoares/algorithms/array/newyearchaos/Solution.java
@@ -1,44 +1,51 @@
 package com.github.filipesoares.algorithms.array.newyearchaos;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Solution {
 	
-	// Complete the minimumBribes function below.
-    static void minimumBribes(int[] q) {
+	static void minimumBribes(int[] q) {
 
+        int ans = 0;
+        String result = "";
+        for (int i = q.length - 1; i >= 0; i--) {
+            if (q[i] - (i + 1) > 2) {
+                result = "Too chaotic";
+                ans = 0;
+                return;
+            }
+            for (int j = Math.max(0, q[i] - 2); j < i; j++)
+                if (q[j] > q[i]) ans++;
+        }
 
+        result = ( (ans > 0) ? String.valueOf(ans) : result );
+
+        System.out.println(result);
     }
 
     public static void main(String[] args) throws IOException {
-        
-    	BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
-        int t = Integer.parseInt(reader.readLine());
-        
-        for (int i = 0; i < t; i++) {
 
-        	int n = Integer.parseInt(reader.readLine());
-			
-			String[] itens = reader.readLine().split(" ");
-			
-			minimumBribes(Arrays
-				.asList(itens)
-				.stream()
-				.mapToInt(Integer::parseInt).toArray());
-			
-		}
-        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        final String[] tests = reader.readLine().split(" ");
+        int t = Integer.parseInt(tests[0]);
+        if ( (1 <= t) && (10 >= t) ){
+            for (int i = 1; i <= t; i++) {
+                final String[] itens = reader.readLine().split(" ");
+                int n = Integer.parseInt(itens[0]);
+                if ( n >= 1 && (n <= Math.pow(10,5) ) ){
+                    int[] q = new int[n];
+                    final String[] numbers = reader.readLine().split(" ");
+
+                    for (int j = 0; j < n; j++) {
+                        q[j] = Integer.parseInt(numbers[j]);
+                    }
+                    minimumBribes(q);
+                }
+            }
+        }
         reader.close();
-        
-        bufferedWriter.flush();
-        bufferedWriter.close();
         
     }
 
